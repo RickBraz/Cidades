1e04a39aff513b7fd6dad5e4c001284c08a4c418
= <=>
2020-05-08 20:40:05 -0300

    adicionei o programa

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Cidades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ubatuba!");
+            Console.ReadKey();
+            Console.WriteLine("caraguatatuba!");
+            Console.ReadKey();
+            Console.WriteLine("ilhabela!");
+            Console.ReadKey();
+            Console.WriteLine("São Sebastião!");
+            Console.ReadKey();
+            Console.WriteLine("Bertioga!");
+            Console.ReadKey();
+            Console.WriteLine("Santos!");
+            Console.ReadKey();
+            Console.WriteLine("Sao Vicente!");
+            Console.ReadKey();
+            Console.WriteLine("Guarujá!");
+            Console.ReadKey();
+            Console.WriteLine("Praia Grande!");
+            Console.ReadKey();
+            Console.WriteLine("Mongaguá!");
+            Console.ReadKey();
+            Console.WriteLine("Itanhaém!");
+            Console.ReadKey();
+            Console.WriteLine("Peruíbe!");
+            Console.ReadKey();
+            Console.WriteLine("Iguape!");
+            Console.ReadKey();
+            Console.WriteLine("Ilha Comprida!");
+            Console.ReadKey();
+            Console.WriteLine("Cananeia!");
+            Console.ReadKey();
+            Console.WriteLine("Cubatão!");
+            Console.ReadKey();
+        }
+    }
+}
